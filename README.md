fc2a094fd392f00faab74e9daed7b4d2ee8889bb
Pozadr <adpozor@gmail.com>
2021-01-22 12:58:58 +0100

    parameterizedTest

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 ### Tutorials:
 * [Spring Boot 2 Academy](https://www.akademiaspring.pl)
 * [Testy jednostkowe w Javie - Istota testów jednostkowych, podstawy JUnit](https://www.youtube.com/watch?v=BijU1qqSH80)
+* [Testy jednostkowe w Javie - TestNG, asercje i testy parametryzowane](https://www.youtube.com/watch?v=EGpGa-o8MkM&list=PLgdDx_cYJqkxcHPhFpElTddgLFXVtMG1p&index=2)
+* [Testy jednostkowe w Javie 2/2 - Tworzenie obiektów imitacji (mocków) w frameworku Mockito](https://www.youtube.com/watch?v=ROBFcefzeJQ)
--- src/test/java/pl/pozadr/hellomockito/workshop/CarServiceTest.java
+++ src/test/java/pl/pozadr/hellomockito/workshop/CarServiceTest.java
@@ -2,46 +2,32 @@ package pl.pozadr.hellomockito.workshop;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 class CarServiceTest {
 
-    @Test
-    void analyzeCarByParams_millageNull_throwsIllegalArgumentException() {
+    @ParameterizedTest
+    @MethodSource("data")
+    void analyzeCarByParams_withDataFromMethod_resultFromData(Integer treadThickness, Integer fuelUsage,
+                                                              Integer carMillage, boolean requiredResult) {
         // given
-        Integer treadThickness = 4;
-        Integer fuelUsage = 5;
-        Integer carMillage = null;
-        CarService carService = new CarService();
-        // when
-        // then
-        Assertions.assertThrows(IllegalArgumentException.class,
-                () -> carService.analyzeCarByParams(treadThickness, fuelUsage, carMillage));
-    }
-
-    @Test
-    void analyzeCarByParams_allValuesOk_resultTrue() {
-        // given
-        Integer treadThickness = 4;
-        Integer fuelUsage = 5;
-        Integer carMillage = 10000;
         CarService carService = new CarService();
         // when
         boolean result = carService.analyzeCarByParams(treadThickness, fuelUsage, carMillage);
         // then
-        Assertions.assertTrue(result);
+        Assertions.assertEquals(requiredResult, result);
     }
 
-    @Test
-    void analyzeCarByParams_carMillageNotOk_resultFalse() {
-        // given
-        Integer treadThickness = 4;
-        Integer fuelUsage = 5;
-        Integer carMillage = 30000;
-        CarService carService = new CarService();
-        // when
-        boolean result = carService.analyzeCarByParams(treadThickness, fuelUsage, carMillage);
-        // then
-        Assertions.assertFalse(result);
+    private static Stream<Arguments> data() {
+        return Stream.of(
+                Arguments.of(4, 5, 10000, true),
+                Arguments.of(0, 14, Integer.MAX_VALUE, false),
+                Arguments.of(4, 5, 30000, false)
+        );
     }
 
     @Test
@@ -58,15 +44,15 @@ class CarServiceTest {
     }
 
     @Test
-    void analyzeCarByParams_valuesEqualsToMinOrMax_resultFalse() {
+    void analyzeCarByParams_millageNull_throwsIllegalArgumentException() {
         // given
-        Integer treadThickness = 0;
-        Integer fuelUsage = 14;
-        Integer carMillage = Integer.MAX_VALUE;
+        Integer treadThickness = 4;
+        Integer fuelUsage = 5;
+        Integer carMillage = null;
         CarService carService = new CarService();
         // when
-        boolean result = carService.analyzeCarByParams(treadThickness, fuelUsage, carMillage);
         // then
-        Assertions.assertFalse(result);
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> carService.analyzeCarByParams(treadThickness, fuelUsage, carMillage));
     }
 }
